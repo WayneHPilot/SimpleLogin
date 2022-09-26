e746b31acb4c72414936e1c41ca02beccb3a3c67
Wayne Pilot <wharveypilot@gmail.com>
2022-09-26 14:27:58 +0100

    Add project files.

---
+++ SimpleLogin/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Login
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var arrUsers = new Users[]
+            {
+            new Users("wayne","admin123"),
+            new Users("dave","demo123"),
+            new Users("steve","me123")
+            };
+
+            Console.WriteLine("Write your username:");
+            var username = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            var password = Console.ReadLine();
+
+            int MaxAttempts = 3;
+
+
+            foreach (Users user in arrUsers)
+            {
+                if (username == user.username && password == user.password)
+                {
+                    Console.WriteLine($"Welcome [username]");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+
+            if (username != "username" || password != "password")
+            {
+                MaxAttempts++;
+
+            }
+            if (MaxAttempts > 2)
+                Console.WriteLine("Login failure");
+        }
+
+
+
+        public class Users
+        {
+            public string username;
+            public string password;
+   
+            public Users(string username, string password)
+            {
+                this.username = username;
+                this.password = password;
+            }
+        }
+    }
+}
